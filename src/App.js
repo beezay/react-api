04c70ec636511f93e1b81cda66fad0e91cb32371
beezay bohora <bijay.okur@gmail.com>
2021-03-05 14:56:07 +0545

    css updated

--- src/App.js
+++ src/App.js
@@ -2,61 +2,61 @@ import { Component } from "react";
 
 import Cardbody from "./components/Cardbody";
 
-//***FETCH TASKS FROM API
-const fetchPosts = async () => {
-  const res = await fetch("http://localhost:5000/posts");
+// //***FETCH TASKS FROM API
+// const fetchPosts = async () => {
+//   const res = await fetch("http://localhost:5000/posts");
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  return data;
-};
+//   return data;
+// };
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
+      url: '',
       fetchData: false,
     };
   }
 
-  componentDidUpdate() {
-    const getPosts = async () => {
-      const postsFromServer = await fetchPosts();
-      //console.log(postsFromServer);
-      this.setState({ posts: postsFromServer });
-    };
-    if (this.state.fetchData && this.state.posts.length === 0) {
-      getPosts();
-      // this.setState({fetchData:false})
-    }
-  }
+  // componentDidUpdate() {
+  //   const getPosts = async () => {
+  //     const postsFromServer = await fetchPosts();
+  //     //console.log(postsFromServer);
+  //     this.setState({ posts: postsFromServer });
+  //   };
+  //   if (this.state.fetchData && this.state.posts.length === 0) {
+  //     getPosts();
+  //     // this.setState({fetchData:false})
+  //   }
+  // }
 
   render() {
     // console.log("From App =>", this.state.posts);
     return (
       <div className="container">
-        <h3>Welcome to React Task</h3>
+        <h3>Select Your Choice</h3>
         <div className="btn-wrapper">
           <button
             className="fetch-btn posts"
-            onClick={() => this.setState({ fetchData: true })}
+            onClick={() => this.setState({ fetchData: true, url:'posts' })}
           >
             Posts
           </button>
           <button
             className="fetch-btn jokes"
-            onClick={() => this.setState({ fetchData: true })}
+            onClick={() => this.setState({ fetchData: true, url:'jokes' })}
           >
-            Posts
+            Jokes
           </button>
           <button
             className="fetch-btn users"
-            onClick={() => this.setState({ fetchData: true })}
+            onClick={() => this.setState({ fetchData: true, url:'users' })}
           >
-            Posts
+            Users
           </button>
         </div>
-        {this.state.fetchData && <Cardbody posts={this.state.posts} />}
+        {this.state.fetchData && <Cardbody url={this.state.url}/>}
       </div>
     );
   }
--- src/components/Cardbody.js
+++ src/components/Cardbody.js
@@ -2,18 +2,48 @@ import React, { Component } from "react";
 import Card from "./Card";
 // import Modal from "./Modal";
 
+//***FETCH TASKS FROM API
+const fetchPosts = async (url) => {
+  const res = await fetch(`http://localhost:5000/${url}`);
+
+  const data = await res.json();
+
+  return data;
+};
 class Cardbody extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      posts: [],
+    };
   }
 
+  // componentDidMount() {
+  //   const getPosts = async (url) => {
+  //     const postsFromServer = await fetchPosts(url);
+  //     //console.log(postsFromServer);
+  //     this.setState({ posts: postsFromServer });
+  //   };
+  //   getPosts(this.props.url);
+  // }
 
+  componentDidUpdate(props, state) {
+    console.log('prop',props, 'state', state)
+    const getPosts = async (url) => {
+      const postsFromServer = await fetchPosts(url);
+      //console.log(postsFromServer);
+      this.setState({ posts: postsFromServer });
+    };
+    if(props.url !== this.props.url || this.state.posts.length === 0) {
+      getPosts(this.props.url);
+    }
+  }
 
   render() {
-    const { posts } = this.props;
-    console.log("Posts=>", posts);
+    const { posts } = this.state;
+    // const { url } = this.props;
+    // console.log("Posts=>", posts);
 
     return (
         <div className="wrapper">
